f891f3716c47f7e1eae2e05aec89de50baeee775
ArithmeticOp <38785647+ArithmeticOp@users.noreply.github.com>
2022-06-01 21:58:40 +0700

    Time: 118 ms (14.63%), Space: 45.1 MB (14.63%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
@@ -1,11 +1,6 @@
 function runningSum(nums: number[]): number[] {
     let sum = [];
     return nums.map((value, index) => {
-        if (sum.length) {
-            return sum[index] = value + sum[index-1];
-        }
-        else {
-            return sum[index] = value;
-        } 
+        return sum[index] = value + (sum[index-1] ?? 0);
     });
 };
