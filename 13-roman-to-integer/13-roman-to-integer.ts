18b4aa23f4b26a3b74417e251835fb0423c5b20a
ArithmeticOp <38785647+ArithmeticOp@users.noreply.github.com>
2022-06-05 13:09:06 +0700

    Time: 246 ms (21.84%), Space: 49 MB (37.69%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.ts
+++ 13-roman-to-integer/13-roman-to-integer.ts
@@ -3,10 +3,11 @@ function romanToInt(s: string): number {
         I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000,
         IV: 4, IX: 9, XL: 40, XC: 90, CD: 400, CM: 900,
     };
+    const sA = s.split("");
     let sum = 0;
-    for (let i = 0; i < s.length; i++) {
-        const now = s[i];
-        const next = s[i+1];
+    for (let i = 0; i < sA.length; i++) {
+        const now = sA[i];
+        const next = sA?.[i+1];
         if (next) {
             const sub = roman[now+next];
             sum += sub ? sub : roman[now];
