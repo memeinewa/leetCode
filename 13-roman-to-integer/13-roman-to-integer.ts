1570d703e85d6683800fb5d07b8827edb9727865
ArithmeticOp <38785647+ArithmeticOp@users.noreply.github.com>
2022-06-05 13:05:38 +0700

    Time: 311 ms (7.57%), Space: 48.6 MB (48.71%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.ts
+++ 13-roman-to-integer/13-roman-to-integer.ts
@@ -1,52 +1,19 @@
 enum roman {
-    I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
+    I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000,
+    IV = 4, IX = 9, XL = 40, XC = 90, CD = 400, CM = 900
 };
 
 function romanToInt(s: string): number {
     let sum = 0;
     for (let i = 0; i < s.length; i++) {
-        const data = condition(s, i);
-        sum += data.value;
-        i = data.index;
+        if (s[i+1]) {
+            const sub = roman[s[i]+s[i+1]];
+            sum += sub ? sub : roman[s[i]];
+            if (sub) i++;
+        }
+        else {
+            sum += roman[s[i]];
+        }
     }
     return sum;
 };
-
-function condition(s: string, index: number): { value: number, index: number} {
-    if (s[index] === "I") {
-        if (s[index + 1] === "V") {
-            index++;
-	        return { value: 4, index };
-        }
-        else if (s[index+1] === "X") {
-            index++;
-            return { value: 9, index };
-        }
-        else return { value: 1, index };
-    }
-    else if (s[index] === "X") {
-        if (s[index + 1] === "L") {
-            index++;
-            return { value: 40, index };
-        }
-        else if (s[index+1] === "C") {
-            index++;
-            return { value: 90, index };
-        }
-        else return { value: 10, index };
-    }
-    else if (s[index] === "C") {
-        if (s[index + 1] === "D") {
-            index++;
-            return { value: 400, index };
-        }
-        else if (s[index+1] === "M") {
-            index++;
-            return { value: 900, index };
-        }
-        else return { value: 100, index };
-    }
-    else {
-        return { value: roman[s[index]], index };
-    }
-}
